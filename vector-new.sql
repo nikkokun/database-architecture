43f30085080f5a7d3733954f5d0b1036b850f3ab
nikkokun <mizutaninikkou@gmail.com>
2018-11-29 15:36:29 +0900

    made modifications to vector_sql

--- vector-new.sql
+++ vector-new.sql
@@ -16,31 +16,31 @@ CREATE TABLE keyword (
 );
 
 INSERT INTO keyword (word, r, g, b)
-VALUES ('hot', 8, 1, 1);
+VALUES ('hot', 8.5, 1.0, 1.0);
 INSERT INTO keyword (word, r, g, b)
-VALUES ('cool', 1, 1, 8);
+VALUES ('cool', 1.0, 1.0, 8.0);
 INSERT INTO keyword (word, r, g, b)
-VALUES ('natural', 1, 8, 1);
+VALUES ('natural', 1.0, 8.0, 1.0);
 INSERT INTO keyword (word, r, g, b)
-VALUES ('fresh', 0, 6, 4);
+VALUES ('fresh', 0.0, 6.0, 4.0);
 INSERT INTO keyword (word, r, g, b)
-VALUES ('colorful', 3, 4, 4);
+VALUES ('colorful', 3.0, 4.0, 4.0);
 
 
 INSERT INTO media (mid, r, g, b)
-VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/sarthou1.gif', 1, 2, 7);
+VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/sarthou1.gif', 1.0, 2.0, 7.0);
 INSERT INTO media (mid, r, g, b)
-VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/hokusai4.gif', 4, 4, 2);
+VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/hokusai4.gif', 4.0, 4.0, 2.0);
 INSERT INTO media (mid, r, g, b)
-VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/gogh2.gif', 2, 5, 3);
+VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/gogh2.gif', 2.0, 5.0, 3.0);
 INSERT INTO media (mid, r, g, b)
-VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/renoir2.gif', 1, 5, 4);
+VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/renoir2.gif', 1.0, 5.0, 4.0);
 INSERT INTO media (mid, r, g, b)
-VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/renoir1.gif', 4, 4, 2);
+VALUES ('http://mdbl-lecture.sfc.keio.ac.jp/lecture/images/renoir1.gif', 4.0, 4.0, 2.0);
 
-DROP FUNCTION IF EXISTS inner_product(int4, int4, int4, int4, int4, int4);
-CREATE FUNCTION inner_product(int4, int4, int4, int4, int4, int4)
-RETURNS int4 AS
+DROP FUNCTION IF EXISTS inner_product(float, float, float, float, float, float);
+CREATE FUNCTION inner_product(float, float, float, float, float, float)
+RETURNS float AS
   'SELECT $1 * $4 + $2 * $5 + $3 * $6 AS inner_product;'
 LANGUAGE 'sql';
 
